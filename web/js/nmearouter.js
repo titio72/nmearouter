969fbafc2fb05ef725b78afd4fd50553a2b8961d
aboni <aboni@aboniPC>
2017-11-16 06:25:20 +0100

    -

--- web/js/nmearouter.js
+++ web/js/nmearouter.js
@@ -239,3 +239,28 @@ function loadStylesheet( url ) {
 	link.media = "screen,print";
 	document.getElementsByTagName( "head" )[0].appendChild( link );
 }
+
+function direction( a ) {
+	if (a<0) a += 180;
+	a = Math.round(a/22.5);
+	switch (a) {
+	case 0: return "N";
+	case 1: return "NNE";
+	case 2: return "NE";
+	case 3: return "ENE";
+	case 4: return "E";
+	case 5: return "ESE";
+	case 6: return "SE";
+	case 7: return "SSE";
+	case 8: return "S";
+	case 9: return "SWW";
+	case 10: return "SW";
+	case 11: return "WSW";
+	case 12: return "W";
+	case 13: return "WNW";
+	case 14: return "NW";
+	case 15: return "NNW";
+	case 16: return "N";
+	}
+	return "";
+}
