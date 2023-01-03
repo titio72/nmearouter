795b9f33dd8a64d0ff971ab2e8a7f3d872c22896
aboni <andrboni@gmil.com>
2023-01-03 20:01:45 +0100

    Misc

--- src/java/com/aboni/nmea/router/TimestampProvider.java
+++ src/java/com/aboni/nmea/router/TimestampProvider.java
@@ -30,7 +30,6 @@ public abstract class TimestampProvider {
      *
      * @param referenceTime The reference time (UNIX time).
      * @param tolerance     The tolerance in ms.
-     * @return true if the time is synced
      * @see TimestampProvider:isSynced()
      */
     public void setSkew(long referenceTime, long tolerance) {
--- src/java/com/aboni/nmea/router/agent/impl/NMEAMetricDBTarget.java
+++ src/java/com/aboni/nmea/router/agent/impl/NMEAMetricDBTarget.java
@@ -30,51 +30,51 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.validation.constraints.NotNull;
 
-public class NMEAMeteoDBTarget extends NMEAAgentImpl {
+public class NMEAMetricDBTarget extends NMEAAgentImpl {
 
     public static final long ONE_MINUTE = 60000L;
     private static final TemperatureSource AIR_TEMPERATURE_SOURCE = TemperatureSource.MAIN_CABIN_ROOM;
 
-    private final Sampler meteoSampler;
+    private final Sampler metricSampler;
     private final Log log;
 
     @Inject
-    public NMEAMeteoDBTarget(@NotNull Log log, @NotNull NMEACache cache, @NotNull TimestampProvider tp,
-                             @NotNull @Named(Constants.TAG_METEO) StatsWriter w) {
+    public NMEAMetricDBTarget(@NotNull Log log, @NotNull NMEACache cache, @NotNull TimestampProvider tp,
+                              @NotNull @Named(Constants.TAG_METEO) StatsWriter w) {
         super(log, tp, false, true);
         this.log = log;
-        meteoSampler = new Sampler(log, tp, w, "Meteo2DB");
-        meteoSampler.initMetric(Metrics.PRESSURE,
+        metricSampler = new Sampler(log, tp, w, "Meteo2DB");
+        metricSampler.initMetric(Metrics.PRESSURE,
                 MsgPressure.class::isInstance,
                 (Message m) -> ((MsgPressure) m).getPressure(),
                 new TimerFilterFixed(5 * ONE_MINUTE, 500),
                 "PR_", 800.0, 1100.0);
-        meteoSampler.initMetric(Metrics.WATER_TEMPERATURE,
+        metricSampler.initMetric(Metrics.WATER_TEMPERATURE,
                 (Message m) -> (m instanceof MsgTemperature && TemperatureSource.SEA == ((MsgTemperature) m).getTemperatureSource()),
                 (Message m) -> ((MsgTemperature) m).getTemperature(),
-                new TimerFilterFixed(10 * ONE_MINUTE, 500),
+                new TimerFilterFixed(30 * ONE_MINUTE, 500),
                 "WT_", -20.0, 60.0);
-        meteoSampler.initMetric(Metrics.AIR_TEMPERATURE,
+        metricSampler.initMetric(Metrics.AIR_TEMPERATURE,
                 (Message m) -> (m instanceof MsgTemperature && AIR_TEMPERATURE_SOURCE == ((MsgTemperature) m).getTemperatureSource()),
                 (Message m) -> ((MsgTemperature) m).getTemperature(),
-                new TimerFilterFixed(10 * ONE_MINUTE, 500),
+                new TimerFilterFixed(30 * ONE_MINUTE, 500),
                 "AT0", -20.0, 60.0);
-        meteoSampler.initMetric(Metrics.HUMIDITY,
+        metricSampler.initMetric(Metrics.HUMIDITY,
                 MsgHumidity.class::isInstance,
                 (Message m) -> ((MsgHumidity) m).getHumidity(),
                 new TimerFilterFixed(10 * ONE_MINUTE, 500),
                 "HUM", 0.0, 150.0);
-        meteoSampler.initMetric(Metrics.WIND_SPEED,
+        metricSampler.initMetric(Metrics.WIND_SPEED,
                 (Message m) -> (m instanceof MsgWindData && ((MsgWindData) m).isTrue()),
                 (Message m) -> ((MsgWindData) m).getSpeed(),
                 new TimerFilterFixed(ONE_MINUTE, 500),
                 "TW_", 0.0, 100.0);
-        meteoSampler.initMetric(Metrics.WIND_DIRECTION,
+        metricSampler.initMetric(Metrics.WIND_DIRECTION,
                 (Message m) -> (m instanceof MsgWindData && ((MsgWindData) m).isTrue() && !cache.isHeadingOlderThan(tp.getNow(), 800)),
                 (Message m) -> ((MsgWindData) m).getAngle() + cache.getLastHeading().getData().getHeading(),
                 new TimerFilterFixed(ONE_MINUTE, 500),
                 "TWD", -360.0, 360.0);
-        meteoSampler.initMetric(Metrics.ROLL,
+        metricSampler.initMetric(Metrics.ROLL,
                 (Message m) -> (m instanceof MsgAttitude && !cache.isHeadingOlderThan(tp.getNow(), 800)),
                 (Message m) -> Utils.normalizeDegrees180To180(((MsgAttitude) m).getRoll()) + HWSettings.getPropertyAsDouble("roll.offset", 0.0),
                 new TimerFilterAnchorAdaptive(cache, ONE_MINUTE, 10 * ONE_MINUTE, 500),
@@ -99,7 +99,7 @@ public class NMEAMeteoDBTarget extends NMEAAgentImpl {
     @Override
     protected boolean onActivate() {
         try {
-            meteoSampler.start();
+            metricSampler.start();
             return true;
         } catch (Exception e) {
             log.errorForceStacktrace(() -> getLogBuilder().wO("activate").toString(), e);
@@ -111,16 +111,16 @@ public class NMEAMeteoDBTarget extends NMEAAgentImpl {
     public void onTimer() {
         super.onTimer();
 
-        meteoSampler.dumpAndReset();
+        metricSampler.dumpAndReset();
     }
 
     @Override
     protected void onDeactivate() {
-        meteoSampler.stop();
+        metricSampler.stop();
     }
 
     @OnRouterMessage
     public void onSentence(RouterMessage msg) {
-        meteoSampler.onSentence(msg);
+        metricSampler.onSentence(msg);
     }
 }
